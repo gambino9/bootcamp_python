0844acd1eeabdfd307143dfaaad560e2e3791cb6
Lamia BOUKROUH <lboukrou@student.42.fr>
2020-07-12 21:59:20 +0200

    d4e6

--- day04/ex06/MyPlotLib.py
+++ day04/ex06/MyPlotLib.py
@@ -6,28 +6,38 @@
 #    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/06/29 21:34:06 by lboukrou          #+#    #+#              #
-#    Updated: 2020/07/05 20:57:21 by lboukrou         ###   ########.fr        #
+#    Updated: 2020/07/12 21:56:56 by lboukrou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # https://www.journaldunet.com/solutions/analytics/1210938-le-feature-engineering-futur-du-data-scientist/
 # https://towardsdatascience.com/understanding-feature-engineering-part-1-continuous-numeric-data-da4e47099a7b
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.density.html
+# https://www.marsja.se/pandas-scatter-matrix-pair-plot/#:~:text=A%20scatter%20matrix%20(pairs%20plot,method%20to%20visualize%20the%20dataset.
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.boxplot.html
 
 from FileLoader import FileLoader
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import scipy as sp
 
 class MyPLotLib():
 	def __init__(self, df, features):
 		pass
 
 	def histograms(self, df, features):
-		pass
+		df[features].hist()
+		plt.show()
 
 	def density(self, df, features):
-		pass
+		df[features].plot.density()
+		plt.show()
 
 	def pair_plot(self, df, features):
-		pass
+		pd.plotting.scatter_matrix(df[features])
+		plt.show()
 
 	def box_plot(self, df, features):
-		pass
+		bx = df.boxplot()
+		plt.show()
--- day04/ex06/test.py
+++ day04/ex06/test.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    test.py                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: lamia <lamia@student.42.fr>                +#+  +:+       +#+         #
+#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/06/29 21:34:17 by lboukrou          #+#    #+#              #
-#    Updated: 2020/07/12 00:51:38 by lamia            ###   ########.fr        #
+#    Updated: 2020/07/12 21:59:09 by lboukrou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,21 +14,30 @@ from FileLoader import FileLoader
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import scipy as sp
 
 df = FileLoader()
 df = df.load("../resources/athlete_events.csv")
 feature1 = 'Height'
 feature2 = "Weight"
+feat = ['Age', 'Height','Weight', 'Year', 'Team', 'NOC', 'Games']
 features = ['Height','Weight']
 # print(df.dtypes)
 
 # HISTOGRAM
-# print(type(features))
 # df[features].hist()
 # plt.show()
 
-# DENSITY (test school)
-# df[feature1].plot.density()
+# DENSITY (both working)
+# df[features].plot.density()
+# ax = df[features].plot.kde()
+# plt.show()
 
 # PAIR PLOT
+# pd.plotting.scatter_matrix(df[features])
+# plt.show()
+
+# BOX PLOT
+# bx = df.boxplot()
+# plt.show()
 
