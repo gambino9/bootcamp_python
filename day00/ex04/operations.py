d53d946a89da4dd19d949b0056fa2c50c758b76c
gambino9 <lboukrou@student.42.fr>
2022-01-06 19:13:02 +0100

    [DEV] d00ex04

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -1,45 +1,26 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    operations.py                                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/04 15:29:57 by lboukrou          #+#    #+#              #
-#    Updated: 2020/03/04 17:33:56 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import sys
 
-# class InputError(Error):
-# 	"""Exception raised for errors in the input
-# 	"""
 
-#	Proposition d'amelioration:
-#
-#	Utiliser des méthodes (ou autre) pour automatiser les cas d'erreurs	
-#	Executer la fonction autrement ?
+def operations(a, b):
+    print("Sum		:", a + b)
+    print("Difference	:", a - b)
+    print("Product		:", a * b)
+    try:
+        print("Quotient	:", a / b)
+        print("Remainder	:", a % b)
+    except ZeroDivisionError:
+        print("Quotient 	: ERROR (div by zero)")
+        print("Remainder 	: ERROR (div by zero)")
 
-def	operations():
-	if len(sys.argv) < 3:
-		print("Usage : python operation.py\nExample :\n\tpython operations.py 10 3")
-	elif len(sys.argv) == 3:
-		try:
-			a, b = int(sys.argv[1]), int(sys.argv[2]) 
-		except ValueError:
-			print("InputError : Only numbers\n\nUsage : python operation.py\nExample :\n\tpython operations.py 10 3")
-			exit()
-		print("Sum		:", a + b)
-		print("Difference	:", a - b)
-		print("Product		:", a * b)
-		try:
-			print("Quotient	:", a / b)
-			print("Remainder	:", a % b)
-		except ZeroDivisionError:
-			print("Quotient 	: ERROR (div by zero)")
-			print("Remainder 	: ERROR (div by zero)")
-	elif len(sys.argv) > 3:
-		print("InputError : Too many arguments\n\nUsage : python operation.py\nExample :\n\tpython operations.py 10 3")
 
-operations()
+if __name__ == "__main__":
+    usage = "Usage : python operation.py <number1> <number2>\nExample :\n\tpython operations.py 10 3"
+    if len(sys.argv) == 3:
+        try:
+            operations(int(sys.argv[1]), int(sys.argv[2]))
+        except ValueError:
+            sys.exit(f"InputError : Only numbers\n\n{usage}")
+    else:
+        if len(sys.argv) > 3:
+            print("InputError : Too many arguments\n")
+        print(usage)
