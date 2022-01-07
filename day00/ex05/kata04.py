5314ac2d0e75d349df705a0347bacec55786a519
gambino9 <lboukrou@student.42.fr>
2022-01-07 14:49:01 +0100

    d00ex05 kata04

--- day00/ex05/kata04.py
+++ day00/ex05/kata04.py
@@ -1,19 +1,5 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    kata04.py                                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/08 19:12:02 by lboukrou          #+#    #+#              #
-#    Updated: 2020/02/11 17:33:49 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-# Way of writing number in scientific notation :
-# print(format(tup[3], "1.2e"))
-# print(format(tup[4], "1.2e"))
+# Expected output format : module_00, ex_04 : 132.42, 1.00e+04, 1.23e+04
 
 tup = (0, 4, 132.42222, 10000, 12345.67)
 
-print("day_0%d, ex_0%d : %.2f," % (tup[0], tup[1], tup[2]), format(tup[3], "1.2e"), ",", format(tup[4], "1.2e"))
+print(f"module_0{tup[0]}, ex_0{tup[1]} : {tup[2]:.2f}, {tup[3]:.2e}, {tup[4]:.2e}")
