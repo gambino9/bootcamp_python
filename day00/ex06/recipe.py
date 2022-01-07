abc43bc68eb6a0ce8daccd88343121954bcc59e2
gambino9 <lboukrou@student.42.fr>
2022-01-07 18:39:02 +0100

    [DEV] d003x06

--- day00/ex06/recipe.py
+++ day00/ex06/recipe.py
@@ -1,77 +1,137 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    recipe.py                                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/11 17:34:28 by lboukrou          #+#    #+#              #
-#    Updated: 2020/02/26 17:33:56 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-#	Getting to know about nested dictionnaries
+import sys
 
 cookbook = {
 	'sandwich': {
 		'ingredients':
-		{
-			'ham',
-			'bread',
-			'cheese',
-			'tomatoes'
-		},
-		'meal': 	'lunch',
+			{
+				'ham',
+				'bread',
+				'cheese',
+				'tomatoes'
+			},
+		'meal': 'lunch',
 		'prep_time': '10',
 	},
-	'cake' :
-	{
-		'ingredients':
+	'cake':
 		{
-			'flour',
-			'suggar',
-			'eggs'
+			'ingredients':
+				{
+					'flour',
+					'sugar',
+					'eggs'
+				},
+			'meal': 'dessert',
+			'prep_time': '60'
 		},
-		'meal': 'dessert',
-		'prep_time': '60'
-	},
-	'salad' :
-	{
-		'ingredients':
+	'salad':
 		{
-			'avocado',
-			'arugula',
-			'tomatoes',
-			'spinach'
-		},
-		'meal': 'lunch',
-		'prep_time': '15'
-	}
+			'ingredients':
+				{
+					'avocado',
+					'arugula',
+					'tomatoes',
+					'spinach'
+				},
+			'meal': 'lunch',
+			'prep_time': '15'
+		}
 }
 
-#	1- How to only print the keys
-for i in cookbook.keys():
-	print(i)
 
-#	2- How to only print the values
-# for i in cookbook.values():
-# 	print(i)
-
-#	3- How to only print the items
-# for i in cookbook.items():
-# 	print(i)
-
-#	Print what's inside the meals
-
-# for k, v in cookbook.items():
-#     for k1, v1 in v.items():
-#         print(k1)
-
-#	Function to print a recipe from cookbook. Param : name recipe
-
-# def print_recipe(cookbook.value()):
-# 	for i in cookbook.values():
-# 		print(i)
+def print_recipe_keys(cookbook_dic):
+	for key in cookbook_dic.keys():
+		print(key)
 
 
+def print_recipe_values(cookbook_dic):
+	for value in cookbook_dic.values():
+		print(value)
 
+
+def print_recipe_items(cookbook_dic):
+	for item in cookbook_dic.items():
+		print(item)
+
+
+def print_recipe(recipe_name):
+	print(f"Recipe for {recipe_name}:\n"
+		  f"Ingredients list : {', '.join(str(x) for x in cookbook[recipe_name]['ingredients'])}\n"
+		  f"To be eaten for {cookbook[recipe_name]['meal']}.\n"
+		  f"Takes {cookbook[recipe_name]['prep_time']} minutes of cooking.")
+
+
+def delete_recipe(recipe_name):
+	cookbook.pop(recipe_name)
+
+
+# name of recipe, ingredients, meal and prep_time
+def add_new_recipe(recipe_name, ingredients: list, meal, prep_time):
+	new = {
+		'ingredients':
+			{
+				f"{a}" for a in ingredients
+			},
+		'meal': meal,
+		'prep_time': prep_time
+	}
+	cookbook[recipe_name] = new
+
+
+def print_all_recipe_names():
+	print("The cookbook contains the following recipes :")
+	print("\n".join(f"{recipe}" for recipe in cookbook.keys()))
+
+
+if __name__ == "__main__":
+	# existing_name = 'sandwich'
+	# new_name = 'omelette au fromage'
+	# new_ingredient = ['eggs', 'fromage', 'pepper']
+	# new_meal = 'lunch'
+	# new_prep_time = 5
+	print("Please select an option by typing the corresponding number\n"
+		  "1: Add a recipe\n"
+		  "2: Delete a recipe\n"
+		  "3: Print a recipe\n"
+		  "4: Print the cookbook\n"
+		  "5: Quit\n")
+	while True:
+		try:
+			choice = int(input())
+			if choice == 1:
+				try:
+					print("To add a new recipe, you must enter the following informations :\n")
+					name = str(input("A name : "))
+					meal = str(input("A meal type : "))
+					time = int(input("The time of preparation : "))
+					nbr_ingredients = int(input("The number of ingredients : "))
+					print("The ingredients : ")
+					# ingredient = list(map(str, input().split()))
+					ingredients = [str(input()) for i in range(nbr_ingredients)]
+					add_new_recipe(name, ingredients, meal, time)
+				except ValueError:
+					pass
+			elif choice == 2:
+				try:
+					recipe_to_del = str(input("Please enter the name of the recipe to delete :\n"))
+					delete_recipe(recipe_to_del)
+				except ValueError:
+					pass
+				except NameError:
+					print("This recipe is not in the cookbook.\n")
+			elif choice == 3:
+				try:
+					recipe_to_print = str(input("Please enter the recipe's name to get its details :\n"))
+					print_recipe(recipe_to_print)
+				except ValueError:
+					pass
+				except NameError:
+					pass
+			elif choice == 4:
+				print_all_recipe_names()
+			elif choice == 5:
+				print("Cookbook closed.\nGood-bye !")
+				sys.exit()
+		except ValueError:
+			print("This option does not exist, please type the corresponding "
+				  "number.\nTo exit, enter 5.\n")
+			continue
