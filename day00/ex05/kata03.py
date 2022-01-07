866df3d89006f0c5d4d0fb929b4edc990878091c
gambino9 <lboukrou@student.42.fr>
2022-01-07 14:41:22 +0100

    [DEV] d00ex05 kata03

--- day00/ex05/kata03.py
+++ day00/ex05/kata03.py
@@ -1,16 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    kata03.py                                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/08 18:50:43 by lboukrou          #+#    #+#              #
-#    Updated: 2020/02/08 19:11:47 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-# A program to pad a string with '-' upto 42, using rjust()
 # https://thispointer.com/python-how-to-pad-strings-with-zero-space-or-some-other-character/
 
 phrase = "The right format"
