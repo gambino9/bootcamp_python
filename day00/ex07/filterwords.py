013beedd4dadf63d71c64d12e87d1af525c41405
gambino9 <lboukrou@student.42.fr>
2022-01-07 19:08:01 +0100

    [DEV] d00ex07

--- day00/ex07/filterwords.py
+++ day00/ex07/filterwords.py
@@ -1,45 +1,12 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    filterwords.py                                     :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/13 19:31:24 by lboukrou          #+#    #+#              #
-#    Updated: 2020/02/22 17:52:57 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-#	Discovering list comprehensions
-#	Note : Have to manage parse of string argument
-
-import string
 import sys
 
-def filterwords():
-	if len(sys.argv) != 3:
-		print("ERROR")
-		exit()
-	try:
-		c = str(sys.argv[1])
-		n = int(sys.argv[2])
-	except ValueError:
-		print("ERROR")
-		exit()
-	no_punct = ""
-	for char in c:
-		if char not in string.punctuation:
-			no_punct = no_punct + char
 
-	hello = no_punct.split()
-	lst = []
-	for word in no_punct.split():
-		if len(word) <= n:
-			continue
-		else:
-			lst.append(word)
-	print(lst)
-
-filterwords()
+def filterwords(string, length):
+	return [word for word in string if len(word) > lg]
 
 
+if __name__ == "__main__":
+	phrase = sys.argv[1].split(" ") if all(elem.isalnum() for elem in sys.argv[1].split()) else sys.exit("ERROR")
+	lg = int(sys.argv[2]) if sys.argv[2].isdigit() else sys.exit("ERROR")
+	new_list = filterwords(phrase, lg)
+	print(new_list)
