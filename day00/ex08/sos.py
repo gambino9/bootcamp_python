6394a98021749b26389514370ca5298b773580fb
Lamia <lboukrou@student.42.fr>
2022-01-08 21:05:27 +0100

    [DEV] d00e08

--- day00/ex08/sos.py
+++ day00/ex08/sos.py
@@ -1,13 +1,60 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    sos.py                                             :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/17 16:22:53 by lboukrou          #+#    #+#              #
-#    Updated: 2020/02/17 16:25:36 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+import sys
 
-#	Check that website : morsecode.scphillips.com/morse.html
+morse_alphabet = {
+	"A": ".-",
+	"B": "-...",
+	"C": "-.-.",
+	"D": "-..",
+	"E": ".",
+	"F": "..-.",
+	"G": "--.",
+	"H": "....",
+	"I": "..",
+	"J": ".---",
+	"K": "-.-",
+	"L": ".-..",
+	"M": "--",
+	"N": "-.",
+	"O": "---",
+	"P": ".--.",
+	"Q": "--.-",
+	"R": ".-.",
+	"S": "...",
+	"T": "-",
+	"U": "..-",
+	"V": "...-",
+	"W": ".--",
+	"X": "-..-",
+	"Y": "-.--",
+	"Z": "--..",
+	" ": "/",
+	"0": "-----",
+	"1": ".----",
+	"2": "..---",
+	"3": "...--",
+	"4": "....-",
+	"5": ".....",
+	"6": "-....",
+	"7": "--...",
+	"8": "---..",
+	"9": "----."
+}
+
+
+def morse_code(string):
+	try:
+		upper_string = string.upper()
+		morse_string = ""
+		for char in upper_string:
+			morse_string += morse_alphabet[char]
+			morse_string += ' '
+		return morse_string[:-1]  # removing last space char
+	except KeyError:
+		return "ERROR"
+
+
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		sys.exit()
+	s = ' '.join(sys.argv[1:])
+	print(morse_code(s))
