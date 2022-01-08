5a0596ff6e5358f9ebfb804ec1f39f4bfd944130
Lamia <lboukrou@student.42.fr>
2022-01-08 21:42:22 +0100

    [DEV] d00e09

--- day00/ex09/guess.py
+++ day00/ex09/guess.py
@@ -1,48 +1,40 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    guess.py                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/17 16:26:09 by lboukrou          #+#    #+#              #
-#    Updated: 2020/02/18 17:08:42 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-#	Interactive guessing number game. Advice : use binary search to solve ;) 
-#	Use of random/randint to generate random int
-#	Proposed improvements : 
-#	- Make small functions for each case
-#	- Do some of the proposed bonuses
-
+import sys
 from random import randint
 
+
 def guess():
-	secret = randint(1,99)
-	print(secret)
-	attempt = 1
-	print("This is an interactive guessing game !\nYou have to enter a number betweem 1 and 99 to find out the secret number.\nType 'exit' to end the game.\nGood luck !")
-	guess = 0
-	while ("guess" != secret):
-		guess = input("What's your guess between 1 and 99 ?\n")
+	secret = randint(1, 99)
+	# print(secret)
+	attempt = 0
+	print(
+		"This is an interactive guessing game !\nYou have to enter a number between 1 and 99 to find out the secret "
+		"number.\nType 'exit' to end the game.\nGood luck !")
+	while True:
+		i = input("What's your guess between 1 and 99 ?\n")
+		guess = None
 		try:
-			guess = int(guess)
+			guess = int(i)
 		except ValueError:
-			if (guess == "exit"):
+			if i == "exit":
 				exit()
 			else:
 				print("That's not a number.")
 				continue
-		if (guess < secret):
+		if guess < secret:
 			print("Too low !")
 			attempt += 1
-		elif (guess > secret):
+		elif guess > secret:
 			print("Too High !")
 			attempt += 1
-		elif (guess == secret):
-			print("Hello")
-			print("Congratulations, you've got it !\nYou won in %d attempts" % attempt)
+		elif guess == secret:
+			if guess == 42:
+				print("The answer to the ultimate question of life, the universe and everything is 42.")
+			if attempt == 0:
+				print("Congratulations! You got it on your first try!")
+			else:
+				print(f"Congratulations, you've got it !\nYou won in {attempt + 1} attempts")
 			break
-	
-guess()
+
+
+if __name__ == "__main__":
+	guess()
