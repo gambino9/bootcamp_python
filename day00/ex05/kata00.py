28235895922c0c57ec1d04bbe69a7957daa76f74
gambino9 <lboukrou@student.42.fr>
2022-01-10 15:32:00 +0100

    [FIX]

--- day00/ex05/kata00.py
+++ day00/ex05/kata00.py
@@ -3,7 +3,7 @@ tup = (19, 42, 21)
 
 def kata00(numbers):
 	print(f"The {len(numbers)} numbers are : {', '.join(str(a) for a in numbers)}")
-	print("the 3 numbers are : %d, %d, %d" % (tup[0], tup[1], tup[2]))
+	# print("the 3 numbers are : %d, %d, %d" % (tup[0], tup[1], tup[2]))
 
 
 if __name__ == "__main__":
