23a0b0286d5d209fb17d873a71c074c5f62ec8dd
gambino9 <lboukrou@student.42.fr>
2022-01-10 16:32:46 +0100

    [FIX] string with punctuation accepted

--- day00/ex07/filterwords.py
+++ day00/ex07/filterwords.py
@@ -1,12 +1,17 @@
+import string
 import sys
 
 
 def filterwords(string, length):
-	return [word for word in string if len(word) > lg]
+	return [word for word in string if len(word) > length]
 
 
 if __name__ == "__main__":
-	phrase = sys.argv[1].split(" ") if all(elem.isalnum() for elem in sys.argv[1].split()) else sys.exit("ERROR")
+	if len(sys.argv) != 3:
+		sys.exit("ERROR")
+
+	phrase_list = sys.argv[1].split(" ") if all(elem.isascii() for elem in sys.argv[1].split()) else sys.exit("ERROR")
 	lg = int(sys.argv[2]) if sys.argv[2].isdigit() else sys.exit("ERROR")
-	new_list = filterwords(phrase, lg)
+	p_list = [s.translate(str.maketrans('', '', string.punctuation)) for s in phrase_list]  # removing punctuation
+	new_list = filterwords(p_list, lg)
 	print(new_list)
