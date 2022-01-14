84eaf4f3fa3e62723a0636ad83ad2f67d3731674
gambino9 <lboukrou@student.42.fr>
2022-01-14 15:57:17 +0100

    [DEV] Vector class constructor

--- day01/ex02/vector.py
+++ day01/ex02/vector.py
@@ -2,43 +2,60 @@
 # - a list of floats : Vector([0.0, 1.0, 2.0, 3.0])
 # - a size of Vector(3) --> vector.values = [0.0, 1.0, 2.0]
 # - a range of Vector((10. 14)) --> vector.values = [10.0, 11.0, 12.0, 13.0]
-class MatrixError(Exception):
+class VectorError(Exception):
 	""" Exception class for Matrix"""
 	pass
 
 
 class Vector:
-	def __init__(self, values=None, *args):
-		self.data = None
+	def __init__(self, arg):
+		self.values = None
 		self.shape = None
-		if len(args) >= 2:
-			raise MatrixError("Too many arguments")
-		elif len(args) == 0:
-			raise MatrixError("Not enough argument")
+		self.row_vector = False
 
-		# TODO : Do the rest
-		if values != None:
-			if type(values) == list:
-				for i in values:
-					if (type(i) != float):
-						print("List must contain floats only")
-						exit()
-				self.values = values
-				self.size = len(values)
-			elif type(values) == int:
-				self.size = values
-				# init = float(0.0)
-				float_list = []
-				for i in range(0, self.size):
-					float_list.append(float(i))
-				self.values = float_list
-			elif type(values) == tuple and len(values) == 2:
-				float_list = [i * 1.0 for i in range(values[0], values[1])]
-				self.values = float_list
-				self.size = len(self.values)
-		elif values == None:
-			print("Please enter values, a size, or a range of vector")
-			exit()
+		if isinstance(arg, (range, tuple, int)):
+			if isinstance(arg, int):
+				float_range = range(0, arg)
+			elif isinstance(arg, tuple):
+				float_range = range(*arg)
+			else:
+				float_range = arg
+			self.values = [[float(elem)] for elem in float_range]
+		elif isinstance(arg, list):
+			if all(isinstance(elem, list) for elem in arg):
+				for sub_list in arg:
+					if not all(isinstance(sub_elem, float) for sub_elem in sub_list):
+						raise VectorError("List must contains floats only")
+			else:
+				if not all(isinstance(elem, float) for elem in arg):
+					raise VectorError("List must contains floats only")
+				self.row_vector = True
+			self.values = arg
+		self.shape = ((1, len(self.values)) if self.row_vector else (len(self.values), 1))
+
+
+		# if values != None:
+		# 	if type(values) == list:
+		# 		for i in values:
+		# 			if (type(i) != float):
+		# 				print("List must contain floats only")
+		# 				exit()
+		# 		self.values = values
+		# 		self.size = len(values)
+		# 	elif type(values) == int:
+		# 		self.size = values
+		# 		# init = float(0.0)
+		# 		float_list = []
+		# 		for i in range(0, self.size):
+		# 			float_list.append(float(i))
+		# 		self.values = float_list
+		# 	elif type(values) == tuple and len(values) == 2:
+		# 		float_list = [i * 1.0 for i in range(values[0], values[1])]
+		# 		self.values = float_list
+		# 		self.size = len(self.values)
+		# elif values == None:
+		# 	print("Please enter values, a size, or a range of vector")
+		# 	exit()
 
 # Overloading many operators here using 'magic methods' (built-in functions) :
 	def __add__(self, other):
