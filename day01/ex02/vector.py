beb5d92c9e06122d6a2bc8d8b92d3c198438b3b8
gambino9 <lboukrou@student.42.fr>
2022-01-14 17:32:40 +0100

    [DEV] Vector class dunder methods add radd sub rsub

--- day01/ex02/vector.py
+++ day01/ex02/vector.py
@@ -1,7 +1,3 @@
-# Creating Vector class that initializes a vector object in 3 differents way :
-# - a list of floats : Vector([0.0, 1.0, 2.0, 3.0])
-# - a size of Vector(3) --> vector.values = [0.0, 1.0, 2.0]
-# - a range of Vector((10. 14)) --> vector.values = [10.0, 11.0, 12.0, 13.0]
 class VectorError(Exception):
 	""" Exception class for Matrix"""
 	pass
@@ -33,84 +29,51 @@ class Vector:
 			self.values = arg
 		self.shape = ((1, len(self.values)) if self.row_vector else (len(self.values), 1))
 
-
-		# if values != None:
-		# 	if type(values) == list:
-		# 		for i in values:
-		# 			if (type(i) != float):
-		# 				print("List must contain floats only")
-		# 				exit()
-		# 		self.values = values
-		# 		self.size = len(values)
-		# 	elif type(values) == int:
-		# 		self.size = values
-		# 		# init = float(0.0)
-		# 		float_list = []
-		# 		for i in range(0, self.size):
-		# 			float_list.append(float(i))
-		# 		self.values = float_list
-		# 	elif type(values) == tuple and len(values) == 2:
-		# 		float_list = [i * 1.0 for i in range(values[0], values[1])]
-		# 		self.values = float_list
-		# 		self.size = len(self.values)
-		# elif values == None:
-		# 	print("Please enter values, a size, or a range of vector")
-		# 	exit()
-
-# Overloading many operators here using 'magic methods' (built-in functions) :
+	# Overloading many operators here using 'magic methods' (built-in functions) :
 	def __add__(self, other):
-		if type(other) == int or type(other) == float:
-			add_list = []
-			for i in self.values:
-				add_list.append(i + other)
-			new_vec = Vector(add_list)
-			print("add")
-			return (new_vec)
-		elif type(other) == Vector:
-			print("add 2 vectors")
-			add_list = []
-			min_size = min(len(self.values), len(other.values))
-			for i in range(0, min_size):
-				add_list.append(self.values[i] + other.values[i])
-			new_vec = Vector(add_list)
-			return (new_vec)
-	
+		""" Only Vectors of same dimension"""
+		if isinstance(other, Vector):
+			if self.shape != other.shape:
+				raise VectorError("The Vectors must be of the same shape")
+			if self.row_vector:
+				vector_data = [a + b for a, b in zip(self.values, other.values)]
+			else:
+				vector_data = [[i + j for i, j in zip(a, b)] for a, b in zip(self.values, other.values)]
+			vector_obj = Vector(vector_data)
+			return vector_obj
+		else:
+			return NotImplemented("Cannot add a Vector with a non Vector object")
+
 	def __radd__(self, other):
-		if type(other) == int or type(other) == float:
-			print("radd")
-			add_list = []
-			for i in self.values:
-				add_list.append(i + other)
-			new_vec = Vector(add_list)
-			return (new_vec)
-	
+		""" Only Vectors of same dimension"""
+		if not isinstance(other, Vector):
+			return NotImplemented("Cannot add a Vector with a non Vector object")
+		else:
+			return other.__add__(self.values)
+
 	def __sub__(self, other):
-		if type(other) == int or type(other) == float:
-			add_list = []
-			for i in self.values:
-				add_list.append(i - other)
-			new_vec = Vector(add_list)
-			print("sub")
-			return (new_vec)
-		elif type(other) == Vector:
-			print("sub 2 vectors")
-			add_list = []
-			min_size = min(len(self.values), len(other.values))
-			for i in range(0, min_size):
-				add_list.append(self.values[i] - other.values[i])
-			new_vec = Vector(add_list)
-			return (new_vec)
-	
+		""" Only Vectors of same dimension"""
+		if isinstance(other, Vector):
+			if self.shape != other.shape:
+				raise VectorError("The Vectors must be of the same shape")
+			if self.row_vector:
+				vector_data = [a - b for a, b in zip(self.values, other.values)]
+			else:
+				vector_data = [[i - j for i, j in zip(a, b)] for a, b in zip(self.values, other.values)]
+			vector_obj = Vector(vector_data)
+			return vector_obj
+		else:
+			return NotImplemented("Cannot sub a Vector with a non Vector object")
+
 	def __rsub__(self, other):
-		if type(other) == int or type(other) == float:
-			print("radd")
-			add_list = []
-			for i in self.values:
-				add_list.append(i - other)
-			new_vec = Vector(add_list)
-			return (new_vec)
+		""" Only Vectors of same dimension"""
+		if not isinstance(other, Vector):
+			return NotImplemented("Cannot rsub a Vector with a non Vector object")
+		else:
+			return other.__sub__(self.values)
 
 	def __truediv__(self, other):
+		""" Only scalars"""
 		if type(other) == int or type(other) == float:
 			add_list = []
 			for i in self.values:
@@ -122,6 +85,7 @@ class Vector:
 			print("Division only with scalars")
 
 	def __rtruediv__(self, other):
+		""" Only scalars"""
 		if type(other) == int or type(other) == scalars:
 			print("rsub")
 			add_list = []
@@ -129,8 +93,9 @@ class Vector:
 				add_list.append(i / other)
 			new_vec = Vector(add_list)
 			return (new_vec)
-		
+
 	def __mul__(self, other):
+		""" Only scalars"""
 		if type(other) == int or type(other) == float:
 			print("mul")
 			add_list = []
@@ -148,6 +113,7 @@ class Vector:
 			return (new_vec)
 
 	def __rmul__(self, other):
+		""" Only scalars"""
 		if type(other) == int or type(other) == float:
 			print("rmul")
 			add_list = []
@@ -160,7 +126,7 @@ class Vector:
 		""" 
 		Link explaining in detail str and repr methods : 
 		https://stackoverflow.com/questions/1436703/difference-between-str-and-repr/2626364#2626364
-		""" 
+		"""
 		pass
 
 	def __repr__(self):
