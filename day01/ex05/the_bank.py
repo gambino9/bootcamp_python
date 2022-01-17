1ca979a8f113de9b1a26487467b1028f2a07a4f1
gambino9 <lboukrou@student.42.fr>
2022-01-17 18:22:12 +0100

    m0105 empty files

---
+++ day01/ex05/the_bank.py
@@ -0,0 +1,40 @@
+# in the_bank.py
+class Bank:
+	"""The bank"""
+
+	def __init__(self):
+		self.account = []
+
+	def add(self, account):
+		self.account.append(account)
+
+	def transfer(self, origin, dest, amount):
+		"""
+			@origin: int(id) or str(name) of the first account
+			@dest: int(id) or str(name) of the destination account
+			@amount: float(amount) amount to transfer
+			@return True if success, False if an error occured
+		"""
+		pass
+
+	def fix_account(self, account):
+		"""
+			fix the corrupted account
+			@account: int(id) or str(name) of the account
+			@return True if success, False if an error occured
+		"""
+		pass
+
+
+class Account:
+	ID_COUNT = 1
+
+	def __init__(self, name, **kwargs):
+		self.id = self.ID_COUNT
+		self.name = name
+		self.__dict__.update(kwargs)
+		Account.ID_COUNT += 1
+
+	def transfer(self, amount):
+		self.value += amount
+
