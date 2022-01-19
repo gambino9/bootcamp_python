2d3255c388899352f28a167407462664a4a6e74a
gambino9 <lboukrou@student.42.fr>
2022-01-19 16:05:16 +0100

    [FIX] Add some explanatory comments

--- day01/ex05/banking_test2.py
+++ day01/ex05/banking_test2.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
         value=1000.0,
         ref='1044618427ff2782f0bbece0abd05f31'
     ))
+    # William's account is corrupted because there is an even number of attributes
     bank.add(Account(
         'William John',
         zip='100-064',
@@ -15,7 +16,7 @@ if __name__ == "__main__":
         ref='58ba2b9954cd278eda8a84147ca73c87',
         info=None
     ))
-
+    # The first transfer fails because William's account is corrupted
     if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
         print('Failed')
 
