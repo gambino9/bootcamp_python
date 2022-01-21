3950ecc56f4b058789882d319456986e2c136caa
gambino9 <lboukrou@student.42.fr>
2022-01-21 19:08:30 +0100

    m02 ex01 squelette

--- day02/ex01/main.py
+++ day02/ex01/main.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    main.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/03/11 18:25:34 by lboukrou          #+#    #+#              #
-#    Updated: 2020/03/12 13:41:11 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 # getattr(object, name[, default])
 # Return the value of the named attribute of object. name must be a string.
 # If the string is the name of one of the object’s attributes, the result is
@@ -24,10 +12,17 @@
 # https://realpython.com/python-kwargs-and-args/
 
 def what_are_the_vars(*args, **kwargs):
+	"""
+		fix the corrupted account
+		@param args:
+		@param kwargs:
+
+		@return: an instance of class ObjectC
+	"""
 	obj = ObjectC()
 	ind = 0
 	for i in args:
-		setattr(obj, "var_"+ str(ind), i)
+		setattr(obj, "var_" + str(ind), i)
 		ind += 1
 	for k, v in kwargs.items():
 		try:
@@ -38,29 +33,32 @@ def what_are_the_vars(*args, **kwargs):
 			return (None)
 	return (obj)
 
+
 class ObjectC(object):
 	def __init__(self):
 		pass
 
-def doom_printer(obj):
-    if obj is None:
-        print("ERROR")
-        print("end")
-        return
-    for attr in dir(obj):
-        if attr[0] != '_':
-            value= getattr(obj, attr)
-            print("{}: {}".format(attr, value))
-    print("end")
 
-if __name__== "__main__":
+def doom_printer(obj):
+	if obj is None:
+		print("ERROR")
+		print("end")
+		return
+	for attr in dir(obj):
+		if attr[0] != '_':
+			value = getattr(obj, attr)
+			print("{}: {}".format(attr, value))
+	print("end")
+
+
+if __name__ == "__main__":
 	obj = what_are_the_vars(7)
 	doom_printer(obj)
-	obj= what_are_the_vars("ft_lol", "Hi")
+	obj = what_are_the_vars("ft_lol", "Hi")
 	doom_printer(obj)
-	obj= what_are_the_vars()
+	obj = what_are_the_vars()
 	doom_printer(obj)
-	obj= what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+	obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
 	doom_printer(obj)
-	obj= what_are_the_vars(42, a=10, var_0="world")
+	obj = what_are_the_vars(42, a=10, var_0="world")
 	doom_printer(obj)
