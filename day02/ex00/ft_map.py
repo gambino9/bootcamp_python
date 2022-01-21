e2c542436f2581f0f69e3443f975a0277a5396bc
gambino9 <lboukrou@student.42.fr>
2022-01-21 17:36:50 +0100

    [DEV] Module02 ex00 ft_map

--- day02/ex00/ft_map.py
+++ day02/ex00/ft_map.py
@@ -1,52 +1,34 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_map.py                                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/03/11 16:10:00 by lboukrou          #+#    #+#              #
-#    Updated: 2020/03/11 17:49:27 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-# The map() function applies a given function to each item of an iterable (list, 
-# tuple etc.) and returns a list of the results.
 # https://book.pythontips.com/en/latest/map_filter.html
-# http://sametmax.com/map-filter-et-reduce/
 
-def ft_map(function_to_apply, list_of_inputs):
+def ft_map(function_to_apply, iterable):
+	"""
+		Map the function to all elements of the iterable.
+		Args:
+		function_to_apply: a function taking an iterable.
+		iterable: an iterable object (list, tuple, iterator).
+		Returns:
+		An iterable.
+		None if the iterable can not be used by the function.
+	"""
 	try:
-		if not list_of_inputs:
-			raise TypeError("ft_map should at least have two parameters")
 		if not callable(function_to_apply):
-			raise TypeError("First parameter is not a function")
-	except TypeError as e:
-		print(e)
-		exit()
-	result = []
-	for i in list_of_inputs:
-		result.append(function_to_apply(i))
-	return (result)
+			raise TypeError("ERROR : First parameter is not a function")
 
-print("----Test Map with a tuple of numbers----")
-def calculateSquare(n):
-    return n*n
+		for i in iterable:
+			yield function_to_apply(i)
+	except TypeError as e:  # A non-iterable type passed as 'iterable' argument will be caught here
+		exit(e)
 
-numbers = (1, 2, 3, 4)
-result = ft_map(calculateSquare, numbers)
-print(result)
 
-print("----Test Map with a list of strings----")
-def ft_upper(string):
-    newstr = ""
-    for c in string:
-        if c.islower():
-            newstr += c.upper()
-        else:
-            newstr += c
-    return newstr
+if __name__ == "__main__":
+	# Example 1:
+	x = [1, 2, 3, 4, 5]
 
-strings = "test", "str", "rokb,vfl"
-result = ft_map(ft_upper, strings)
-print(result)
+	print(ft_map(lambda dum: dum + 1, x))
+	# Output:
+	# < generator object ft_map at 0x7f708faab7b0 >  # The adress will be different
+
+	print(list(ft_map(lambda t: t + 1, x)))
+	print(list(ft_map(lambda t: t + 1, x)))
+	# Output:
+	# [2, 3, 4, 5, 6]
