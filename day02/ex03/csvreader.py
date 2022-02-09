23e5bdb76ffd1fbc67d411ac5d4f05892d072c72
gambino9 <lboukrou@student.42.fr>
2022-02-09 19:01:29 +0100

    [DEV] d02ex03 Csvreader done

--- day02/ex03/csvreader.py
+++ day02/ex03/csvreader.py
@@ -1,87 +1,8 @@
 # http://sametmax.com/les-context-managers-et-le-mot-cle-with-en-python/
 # https://zestedesavoir.com/tutoriels/954/notions-de-python-avancees/3-further/2-context-managers/
-
 import pathlib
 import csv
 
-
-# class CsvReader:
-# 	def __init__(self, filename, sep=',', header=False, skip_top=0, skip_bottom=0):
-# 		self.filename = filename
-# 		self.sep = sep
-# 		self.header = header
-# 		self.skip_top = skip_top
-# 		self.skip_bottom = skip_bottom
-#
-# 		self.file = None
-# 		self.data = []
-# 		self.head = None
-#
-# 	def __enter__(self):
-# 		print("Enter")
-# 		try:
-# 			self.file = open(self.filename)
-# 			if self.header is True:
-# 				self.head = self.file.readline()
-# 			for i in range(0, self.skip_top - 1):
-# 				self.file.readline()
-# 				print(self.file.readline())
-# 			with open(self.filename) as my_file:
-# 				lines_count = (sum(1 for _ in my_file))
-# 				# print(f"{lines_count=}")
-# 			length = 0
-# 			for i in range(self.skip_top, lines_count - self.skip_bottom):
-# 				line = list(filter(None, self.file.readline().strip().split(self.sep)))
-# 				# line = self.file.readline().split(self.sep)
-# 				if length == 0:
-# 					length = len(line)
-# 				assert length == len(line)
-# 				# print(line)
-# 				self.data.append(line)
-# 		except:
-# 			print("Error")
-# 		# finally:
-# 		# 	return self.file
-# 		# self.file = open(self.filename)
-# 		return self
-#
-# 	def __exit__(self, type, value, traceback):
-# 		print("Close")
-# 		self.file.close()
-#
-# 	def get_data(self):
-# 		"""
-# 			Retrieves the data/records from skip_top to skip bottom.
-# 			Returns:
-# 			nested list (list(list, list, ...)) representing the data.
-# 		"""
-# 		print('plop')
-# 		self.file = open(self.filename)
-# 		# print(self.file.readline())
-# 		print(list(self.file.readline().split(self.sep)))
-# 		if self.header is True:
-# 			self.head = self.file.readline()
-# 		for i in range(0, self.skip_top - 1):
-# 			self.file.readline()
-# 		with open(self.filename) as my_file:
-# 			lines_count = (sum(1 for _ in my_file))
-# 			# print(f"{lines_count=}")
-# 		length = 0
-# 		for i in range(self.skip_top, lines_count - self.skip_bottom):
-# 			line = list(filter(None, self.file.readline().strip().split(self.sep)))
-# 			# line = self.file.readline().split(self.sep)
-# 		return self.data
-#
-# 	def get_header(self):
-# 		"""
-# 			Retrieves the header from csv file.
-#
-# 			@return:
-# 			list: representing the data (when self.header is True).
-# 			None: (when self.header is False).
-# 		"""
-# 		print('pouloulou')
-# 		return self.head
 
 class CsvReader:
 	def __init__(self, filename, sep=',', header=False, skip_top=0, skip_bottom=0):
@@ -95,85 +16,39 @@ class CsvReader:
 		self.data = []
 		self.head = None
 		self.file_path = None
-		self.row_len = 0
 
 	def __enter__(self):
-		print("Enter")
 		self.file_path = pathlib.Path(self.filename)
 		self.file = open(self.file_path)
 		reader = csv.reader(self.file)
 
-		self.row_len = len(list(reader))
-		print(self.row_len)
-		print(list(reader))
 		if self.header:
 			self.head = next(reader)
-		for i in range(self.skip_top):
-			next(reader)
-		# print(list(reader))
-		# if self.skip_bottom:
-		# print(self.row_len - self.skip_bottom)
-		for i in range(self.row_len - self.skip_bottom):
-			# print(list(next(reader)))
-			self.data.append(list(next(reader)))
-		# print(f"blob{next(reader)}")
-		# print(self.data)
+		self.data = [i for i in reader]
+
+		# Check if all rows are the same size as the header
+		# At each iteration, the row is converted to a string to check its length
+		# We also check for empty elements in list
+		for sub_list in self.data:
+			if '' in sub_list:
+				return None
+			if not len(','.join(sub_list).split(self.sep)) == len(self.data[0]):
+				return None
 		return self
-		# try:
-		# 	self.file = open(self.filename)
-		# 	if self.header is True:
-		# 		self.head = self.file.readline()
-		# 	for i in range(0, self.skip_top - 1):
-		# 		self.file.readline()
-		# 		print(self.file.readline())
-		# 	with open(self.filename) as my_file:
-		# 		lines_count = (sum(1 for _ in my_file))
-		# 		# print(f"{lines_count=}")
-		# 	length = 0
-		# 	for i in range(self.skip_top, lines_count - self.skip_bottom):
-		# 		line = list(filter(None, self.file.readline().strip().split(self.sep)))
-		# 		# line = self.file.readline().split(self.sep)
-		# 		if length == 0:
-		# 			length = len(line)
-		# 		assert length == len(line)
-		# 		# print(line)
-		# 		self.data.append(line)
-		# except:
-		# 	print("Error")
-		# # finally:
-		# # 	return self.file
-		# # self.file = open(self.filename)
-		# return self
 
 	def __exit__(self, type, value, traceback):
-		print("Close")
 		self.file.close()
 
-	def get_data(self):
+	def getdata(self):
 		"""
 			Retrieves the data/records from skip_top to skip bottom.
 			Returns:
 			nested list (list(list, list, ...)) representing the data.
 		"""
-		# print('plop')
-		# self.file = open(self.filename)
-		# # print(self.file.readline())
-		# print(list(self.file.readline().split(self.sep)))
-		# if self.header is True:
-		# 	self.head = self.file.readline()
-		# for i in range(0, self.skip_top - 1):
-		# 	self.file.readline()
-		# with open(self.filename) as my_file:
-		# 	lines_count = (sum(1 for _ in my_file))
-		# 	# print(f"{lines_count=}")
-		# length = 0
-		# for i in range(self.skip_top, lines_count - self.skip_bottom):
-		# 	line = list(filter(None, self.file.readline().strip().split(self.sep)))
-		# 	# line = self.file.readline().split(self.sep)
+		self.data = self.data[self.skip_top:-self.skip_bottom if self.skip_bottom != 0 else None]
 		return self.data
 
-
-	def get_header(self):
+	def getheader(self):
 		"""
 			Retrieves the header from csv file.
 
@@ -181,20 +56,29 @@ class CsvReader:
 			list: representing the data (when self.header is True).
 			None: (when self.header is False).
 		"""
-		print('pouloulou')
+		if self.header is False:
+			return None
 		return self.head
+
+	def __str__(self):
+		return '\n'.join(str(i) for i in self.data)
 
 
 if __name__ == "__main__":
-	# c = CsvReader('good.csv')
-	with CsvReader('good.csv', header=True, skip_top=17) as file:
-		data = file.get_data()
-		print(data)
-		header = file.get_header()
-		# print(data)
-		# print(header)
+	with CsvReader('good.csv', skip_top=18, skip_bottom=0) as file:
+		if file is None:
+			print("File is corrupted or missing")
+		else:
+			print(file.getheader(), end="\n")
+			print(file.getdata(), end="\n\n")
 
-# if __name__ == "__main__":
-#     with CsvReader('bad.csv') as file:
-#         if file == None:
-#             print("File is corrupted")
+	with CsvReader('good.csv', header=True, skip_top=17, skip_bottom=0) as file:
+		if file is None:
+			print("File is corrupted or missing")
+		else:
+			print(file.getheader(), end="\n")
+			print(file.getdata(), end="\n\n")
+
+	with CsvReader('bad.csv') as file:
+		if file is None:
+			print("File is corrupted")
