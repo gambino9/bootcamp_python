8751ae4d2d56443b8395d00d737339eee4aa55db
gambino9 <lboukrou@student.42.fr>
2022-02-22 17:17:24 +0100

    [DEV] ImageProcessor done

--- day03/ex01/ImageProcessor.py
+++ day03/ex01/ImageProcessor.py
@@ -1,22 +1,8 @@
-
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ImageProcessor.py                                  :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/03/12 19:55:19 by lboukrou          #+#    #+#              #
-#    Updated: 2020/03/12 20:10:52 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 # https://stackoverflow.com/questions/20597088/display-a-png-image-from-python-on-mint-15-linux
 # http://www.degeneratestate.org/posts/2016/Oct/23/image-processing-with-numpy/
 
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+
 
 class ImageProcessor():
 	def __init__(self):
@@ -30,13 +16,9 @@ class ImageProcessor():
 		img = plt.imread(path)
 		y = img.shape[0]
 		x = img.shape[1]
-		print("Loading image of dimensions {} x {}".format(x, y))
-		# print(y)
-		# print(x)
-		return (img)
-		
+		print(f"Loading image of dimensions {x} x {y}")
+		return img
 
 	def display(self, array):
 		plt.imshow(array)
 		plt.show() 
-		pass
--- day03/ex01/test.py
+++ day03/ex01/test.py
@@ -1,18 +1,7 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    test.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/03/19 19:21:11 by lamia             #+#    #+#              #
-#    Updated: 2020/06/08 18:22:18 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 from ImageProcessor import ImageProcessor
 
 imp = ImageProcessor()
 arr = imp.load("../42AI.png")
 print(arr)
-# imp.display(arr)
+# print(type(arr))
+imp.display(arr)
