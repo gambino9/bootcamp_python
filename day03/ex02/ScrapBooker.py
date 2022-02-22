55566261f0e855e9197265d09fb0207881a7745c
gambino9 <lboukrou@student.42.fr>
2022-02-22 19:29:11 +0100

    [DEV] Scrapbooker : Crop method OK, juxtapose not correct

--- day03/ex02/ScrapBooker.py
+++ day03/ex02/ScrapBooker.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ScrapBooker.py                                     :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/03/23 15:54:49 by lamia             #+#    #+#              #
-#    Updated: 2020/06/16 20:25:32 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 # https://www.tutorialspoint.com/numpy/numpy_indexing_and_slicing.htm
 # https://note.nkmk.me/en/python-numpy-ndarray-ndim-shape-size
 # https://machinelearningmastery.com/index-slice-reshape-numpy-arrays-machine-learning-python/
@@ -18,46 +6,52 @@
 # https://numpy.org/doc/1.18/reference/generated/numpy.concatenate.html
 # https://numpy.org/doc/stable/reference/generated/numpy.tile.html
 
+import sys
+
 import numpy as np
 
-class ScrapBooker():
-    def __init__(self):
-        pass
-    
-    def crop(self, array, dimensions, position):
-        """ crop the image as a rectangle with the given dimensions
-        (meaning, the new height and width for the image), whose top left
-        corner is given by the position argument.
-        The position should be (0,0) by default. You have to consider it an
-        error (and handle said error) if dimensions is larger than the current
-        image size."""
-        try:
-            if dimensions > array.shape:
-                raise TypeError("dimensions must be inferior to array's dimensions")
-        except TypeError as e:
-            print(e)
-            exit()
-        height = dimensions[0]
-        width = dimensions[1]
-        arr = np.array(array)
-        newArr = arr[:height,width]
-        return (newArr)
 
-    def thin(self, array, n, axis):
-        """ delete every n-th pixel row along the specified axis
-        (0 vertical, 1 horizontal)."""
-        axis = 1 - axis
-        arr = np.delete(array, np.s_[n-1::n], axis)
-        return (arr)
+class ScrapBooker:
+	def __init__(self):
+		pass
 
-    def juxtapose(self, array, n, axis):
-        """ juxtapose n copies of the image along the specified axis
-        (0 vertical, 1 horizontal)""" 
-        axis = 1 - axis
-        return (np.concatenate([array] * n, axis))
+	def crop(self, array, dim, position=(0, 0)):
+		""" crop the image as a rectangle with the given dimensions
+		(meaning, the new height and width for the image), whose top left
+		corner is given by the position argument.
+		The position should be (0,0) by default. You have to consider it an
+		error (and handle said error) if dimensions is larger than the current
+		image size."""
+		try:
+			if not all(isinstance(arg, tuple) for arg in [dim, position]):
+				raise TypeError("Dimensions and position must be tuples")
+			if any(i < 0 for i in dim) or any(j < 0 for j in position):
+				raise TypeError("Dimensions and position must not be negative numbers")
+			if not all(x > y for x, y in zip(array.shape, dim)):
+				raise TypeError("Dimensions must be inferior to array's dimensions")
+			row = dim[0]
+			column = dim[1]
+			arr = np.array(array)
+			new_array = arr[position[0]:(position[0] + row), position[1]:(position[1] + column)]
+			return new_array
+		except TypeError as e:
+			sys.exit(e)
 
-    def mosaic(self, array, dimensions):
-        """ make a grid with multiple copies of the array.
-        The dimensions argument specifies the dimensions (meaning
-        the height and width) of the grid (e.g. 2x3)."""
-        return (np.tile(array, (dimensions)))
+	def thin(self, array, n, axis):  # TODO : Seems to work, gotta check though
+		""" delete every n-th pixel row along the specified axis
+		(0 vertical, 1 horizontal)."""
+		axis = 1 - axis
+		arr = np.delete(array, np.s_[n-1::n], axis)
+		return arr
+
+	def juxtapose(self, array, n, axis):  # TODO : This one gotta be checked, does not work properly
+		""" juxtapose n copies of the image along the specified axis
+		(0 vertical, 1 horizontal)"""
+		axis = 1 - axis
+		return np.concatenate([array] * n, axis)
+
+	def mosaic(self, array, dim):
+		""" make a grid with multiple copies of the array.
+		The dimensions argument specifies the dimensions (meaning
+		the height and width) of the grid (e.g. 2x3)."""
+		return np.tile(array, dim)
--- day03/ex02/test.py
+++ day03/ex02/test.py
@@ -1,30 +1,39 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    test.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/03/25 15:36:10 by lamia             #+#    #+#              #
-#    Updated: 2020/06/16 19:19:51 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import numpy as np
 from ScrapBooker import ScrapBooker
 
-a = np.array([['A','A','A','A','A','A','A','A','A','A','A','A'],['B','B','B','B','B','B','B','B','B','B','B','B'],['C','C','C','C','C','C','C','C','C','C','C','C'], ['D','D','D','D','D','D','D','D','D','D','D','D'], ['E','E','E','E','E','E','E','E','E','E','E','E'], ['F','F','F','F','F','F','F','F','F','F','F','F'], ['G','G','G','G','G','G','G','G','G','G','G','G'], ['H','H','H','H','H','H','H','H','H','H','H','H']])
-b = np.array([['A','B','C','D','E','F','G','H','I','J','K','L'],['A','B','C','D','E','F','G','H','I','J','K','L'],['A','B','C','D','E','F','G','H','I','J','K','L'], ['A','B','C','D','E','F','G','H','I','J','K','L'], ['A','B','C','D','E','F','G','H','I','J','K','L'], ['A','B','C','D','E','F','G','H','I','J','K','L'], ['A','B','C','D','E','F','G','H','I','J','K','L'], ['A','B','C','D','E','F','G','H','I','J','K','L']])
+a = np.array([['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'],
+			  ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+			  ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'],
+			  ['D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'],
+			  ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+			  ['F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'],
+			  ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'],
+			  ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H']])
 
-# CROP
+b = np.array([['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+			  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+			  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+			  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+			  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+			  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+			  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+			  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']])
 
-# print (b)
-# print("array shape is {}".format(b.shape))
-# print("------------")
-# split = 5
-# train,test = b[:split,:8],b[split:,:]
-# print(train)
-# print(test)
+# # CROP
+# print(b)
+# pos = (2, 2)
+# dimension = (1, 5)
+# c = b[pos[0]:(pos[0] + dimension[0]), pos[1]:(pos[1] + dimension[1])]
+# print(c)
+# print(c.shape)
+
+spb = ScrapBooker()
+# print(arr1 := np.arange(0,25).reshape(5,5))
+# print(spb.crop(arr1, (3,1),(1,0)))
+# # Output
+# # array([[ 5],
+# # 	   [10],
+# # 	   [15]])
 
 # THIN
 
@@ -36,21 +45,52 @@ b = np.array([['A','B','C','D','E','F','G','H','I','J','K','L'],['A','B','C','D'
 # print("array shape of a_del is {}".format(a_del.shape))
 # print("------------")
 
+# print(arr2 := np.array("A B C D E F G H I".split() * 6).reshape(-1,9))
+# print(spb.thin(arr2,3,0))
+# print(spb.thin(arr2,3,0).dtype)
+# # Output :
+# # array([['A', 'B', 'D', 'E', 'G', 'H'],
+# # 	   ['A', 'B', 'D', 'E', 'G', 'H'],
+# # 	   ['A', 'B', 'D', 'E', 'G', 'H'],
+# # 	   ['A', 'B', 'D', 'E', 'G', 'H'],
+# # 	   ['A', 'B', 'D', 'E', 'G', 'H'],
+# # 	   ['A', 'B', 'D', 'E', 'G', 'H']], dtype='<U1')
+
+# print(arr3 := np.array([[var] * 10 for var in "ABCDEFG"]))
+# print(spb.thin(arr3,3,1))
+# print(spb.thin(arr3,3,1).dtype)
+#
+# # Output :
+# # array([['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'],
+# # 	   ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'],
+# # 	   ['D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'],
+# # 	   ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
+# # 	   ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G']], dtype='<U1')
+
 # JUXTAPOSE & MOSAIC
 
-a1 = np.array([[1, 2, 3],
-            	[4, 5, 6],
-				[7, 8, 9]])
-n = 3
-print(a1)
-print("------------")
-# a2 = np.concatenate([a1] * n,0)
-a2 = np.tile(a1, (4,3))
-print("------------")
-# a2 = np.tile(a1,2)
-print(a2)
+# a1 = np.array([[1, 2, 3],
+# 				[4, 5, 6],
+# 				[7, 8, 9]])
+# n = 3
+# print(a1)
+# print("------------")
+# # a2 = np.concatenate([a1] * n,0)
+# a2 = np.tile(a1, (4, 3))
+# print("------------")
+# # a2 = np.tile(a1,2)
+# print(a2)
 
+print(arr4 := np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]]))
+print(spb.juxtapose(arr4, 2, 0))
 
+# Output :
+# array([[1, 2, 3],
+# 	   [4, 5, 6],
+# 	   [7, 8, 9],
+# 	   [1, 2, 3],
+# 	   [4, 5, 6],
+# 	   [7, 8, 9]])
 
 # 3 different examples of using reshape to turn arrays into 1 2 or 3D :
 
