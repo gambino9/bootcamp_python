55c50b48a9c774be022e6334b2d3cd9c4a39653c
gambino9 <lboukrou@student.42.fr>
2022-02-23 16:54:28 +0100

    [DEV] Color filter methods done

--- day03/ex03/ColorFilter.py
+++ day03/ex03/ColorFilter.py
@@ -1,33 +1,108 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ColorFilter.py                                     :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/16 20:19:42 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/20 19:40:38 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+# https://yashaslokesh.github.io/inverting-the-colors-of-an-image.html
 
 import numpy as np
 
-class ColorFilter():
+
+class ColorFilter:
 	def __init__(self):
 		pass
 	
 	def invert(self, array):
-		return 1 - array
-		pass
+		"""
+		Inverts the color of the image received as a numpy array.
+		Args:
+		array: numpy.ndarray corresponding to the image.
+		Return:
+		array: numpy.ndarray corresponding to the transformed image.
+		None: otherwise.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		colors_arr = array[:, :, :3]  # Takes color data from image array, without last channel
+		invert_col_arr = 1 - colors_arr
+
+		return invert_col_arr
 
 	def to_blue(self, array):
-		pass
+		"""
+		Applies a blue filter to the image received as a numpy array.
+		Args:
+		array: numpy.ndarray corresponding to the image.
+		Return:
+		array: numpy.ndarray corresponding to the transformed image.
+		None: otherwise.
+		Raises:
+		This function should not raise any Exception.
+		◦ Authorized functions:.zeros,.shape,.dstack
+		◦ Authorized operator:None
+		"""
+		colors_arr = np.zeros((len(array), len(array[0]), 3))
+		colors_arr[:,:,2] = array[:, :, 2]
+
+		return colors_arr
 
 	def to_green(self, array):
-		pass
+		"""
+		Applies a green filter to the image received as a numpy array.
+		Args:
+		array: numpy.ndarray corresponding to the image.
+		Return:
+		array: numpy.ndarray corresponding to the transformed image.
+		None: otherwise.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		colors_arr = np.zeros((len(array), len(array[0]), 3))
+		colors_arr[:,:,1] = array[:, :, 1]
+
+		return colors_arr
 
 	def to_red(self, array):
-		pass
+		"""
+		Applies a red filter to the image received as a numpy array.
+		Args:
+		array: numpy.ndarray corresponding to the image.
+		Return:
+		array: numpy.ndarray corresponding to the transformed image.
+		None: otherwise.
+		Raises:
+		This function should not raise any Exception.
+		"""
+		colors_arr = np.zeros((len(array), len(array[0]), 3))
+		colors_arr[:,:,0] = array[:, :, 0]
+
+		return colors_arr
 
 	def celluloid(self, array):
-		pass
+		"""
+		Applies a celluloid filter to the image received as a numpy array.
+		Celluloid filter must display at least four thresholds of shades.
+		Be careful! You are not asked to apply black contour on the object,
+		you only have to work on the shades of your images.
+		Remarks:
+		celluloid filter is also known as cel-shading or toon-shading.
+		Args:
+		array: numpy.ndarray corresponding to the image.
+		Return:
+		array: numpy.ndarray corresponding to the transformed image.
+		None: otherwise.
+		Raises:
+		This function should not raise any Exception.
+		"""
+
+	def to_grayscale(array, filter, **kwargs):
+		"""
+		Applies a grayscale filter to the image received as a numpy array.
+		For filter = ’mean’/’m’: performs the mean of RBG channels.
+		For filter = ’weight’/’w’: performs a weighted mean of RBG channels.
+		Args:
+		array: numpy.ndarray corresponding to the image.
+		filter: string with accepted values in [’m’,’mean’,’w’,’weight’]
+		weights: [kwargs] list of 3 floats where the sum equals to 1,
+		corresponding to the weights of each RBG channels.
+		Return:
+		array: numpy.ndarray corresponding to the transformed image.
+		None: otherwise.
+		Raises:
+		This function should not raise any Exception.
+		"""
