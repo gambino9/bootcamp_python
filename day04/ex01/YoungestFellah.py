0cfaf1d0f44a95435b257d43fd3eee05c30412ac
Lamia <lboukrou@student.42.fr>
2022-06-30 12:29:53 +0200

    [DEV] module04 ex01

--- day04/ex01/YoungestFellah.py
+++ day04/ex01/YoungestFellah.py
@@ -1,25 +1,9 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    YoungestFellah.py                                  :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/20 20:52:15 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/24 18:30:21 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 # http://www.datasciencemadesimple.com/get-minimum-value-column-python-pandas/
 
-import sys
-sys.path.append('../ex00')
-from FileLoader import FileLoader
-import pandas as pd
 
-def YoungestFellah(df, year):
+def youngest_fellah(df, year):
 	"""
-	The function returns a dictionary containing the age of the youngest 
+	The function returns a dictionary containing the age of the youngest
 	woman and man who took part in the Olympics on that year.
 	"""
 	data_year = df[df.Year == year]
@@ -27,5 +11,5 @@ def YoungestFellah(df, year):
 	data_men = data_year[data_year.Sex == 'M']
 	min_fem = data_fem['Age'].min()
 	min_men = data_men['Age'].min()
-	d = {'f' : min_fem, 'm' : min_men}
-	return (d)
+	d = {'f': min_fem, 'm': min_men}
+	return d
--- day04/ex01/test.py
+++ day04/ex01/test.py
@@ -1,26 +1,18 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    test.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/21 18:30:07 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/24 18:31:55 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 from FileLoader import FileLoader
-from YoungestFellah import YoungestFellah
-import pandas as pd
+from YoungestFellah import youngest_fellah
 
 loader = FileLoader()
 data = loader.load('../resources/athlete_events.csv')
-year = 2004
-data_year = data[data.Year == year]
-fem = data_year[data_year.Sex == 'F']
-men = data_year[data_year.Sex == 'M']
-print(fem['Age'].min())
-print(men['Age'].min())
-d = {'f' : fem['Age'].min(), 'm' : men['Age'].min()}
-print(type(d))
+year = 1992
+year_1 = 2004
+year_2 = 2010
+year_3 = 2003
+
+res = youngest_fellah(data, year)
+res_1 = youngest_fellah(data, year_1)
+res_2 = youngest_fellah(data, year_2)
+res_3 = youngest_fellah(data, year_3)
+print(f"Youngest woman and man in the year {year} are {res}")
+print(f"Youngest woman and man in the year {year_1} are {res_1}")
+print(f"Youngest woman and man in the year {year_2} are {res_2}")
+print(f"Youngest woman and man in the year {year_3} are {res_3}")
