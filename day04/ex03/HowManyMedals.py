06e7a474646653d831053b4bd27fcb1948b6c288
Lamia <lboukrou@student.42.fr>
2022-07-01 15:30:03 +0200

    [DEV] module04 ex03

--- day04/ex03/HowManyMedals.py
+++ day04/ex03/HowManyMedals.py
@@ -1,21 +1,27 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    HowManyMedals.py                                   :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/27 19:10:20 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/28 19:38:35 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/missing_data.html
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.dropna.html
 # https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
 
-from FileLoader import FileLoader
 import pandas as pd
 
-def HowManyMedals(df, name):
-	pass
+
+def how_many_medals(df, name):
+    """
+    The function returns a dictionary of dictionaries giving the number and
+    type of medals for each year during which the participant won medals.
+    """
+    if not isinstance(df, pd.DataFrame) or not isinstance(name, str):
+        return None
+
+    # Keep the rows containing only the name
+    data_years = df[df.Name == name]
+    # data_years = data_years.dropna()
+    years = data_years['Year'].unique()
+    total = {}
+    for year in years:
+        # Keeping the rows containing only the year for each year iterated
+        dy = data_years[data_years.Year == int(year)]
+        total[year] = {'G': len(dy[dy.Medal == 'Gold']),
+                       'S': len(dy[dy.Medal == 'Silver']),
+                       'B': len(dy[dy.Medal == 'Bronze'])}
+    return total
--- day04/ex03/test.py
+++ day04/ex03/test.py
@@ -1,26 +1,30 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    test.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/27 20:36:59 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/28 19:25:49 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-from HowManyMedals import HowManyMedals
+from HowManyMedals import how_many_medals
 from FileLoader import FileLoader
-import pandas as pd
 
 loader = FileLoader()
 df = loader.load('../resources/athlete_events.csv')
 name = 'Kjetil Andr Aamodt'
-data_years = df[df.Name == name]
-print(data_years)
-data_years = data_years.dropna()
-dic = {}
-dic = data_years.to_dict()
-print(dic)
-gold, silver, bronze = 0, 0, 0
+name_1 = 'Gary Abraham'
+name_2 = 'Yekaterina Konstantinovna Abramova'
+name_3 = 'Kristin Otto'
+medals = how_many_medals(df, name)
+medals_1 = how_many_medals(df, name_1)
+medals_2 = how_many_medals(df, name_2)
+medals_3 = how_many_medals(df, name_3)
+
+print(medals)
+print(name)
+for i in medals:
+    print(i, medals[i])
+
+print(name_1)
+for i in medals_1:
+    print(i, medals_1[i])
+
+print(name_2)
+for i in medals_2:
+    print(i, medals_2[i])
+
+print(name_3)
+for i in medals_3:
+    print(i, medals_3[i])
