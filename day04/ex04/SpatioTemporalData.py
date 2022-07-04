9d24d305ef2b73cedfafb6d176c7bccfe28eba3b
Lamia <lboukrou@student.42.fr>
2022-07-04 15:50:23 +0200

    [DEV] module04 ex04

--- day04/ex04/SpatioTemporalData.py
+++ day04/ex04/SpatioTemporalData.py
@@ -1,48 +1,33 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    SpatioTemporalData.py                              :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/28 19:35:25 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/29 20:52:17 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 # https://www.geeksforgeeks.org/python-extracting-rows-using-pandas-iloc/
 # https://stackoverflow.com/questions/22341271/get-list-from-pandas-dataframe-column
 
-from FileLoader import FileLoader
-import pandas as pd
 
-"""
-Write a class calledSpatioTemporalDatawhich takes a dataset (pandas DataFrame)
-as argument in itsconstructor and implements the following methods:
-"""
 class SpatioTemporalData:
 	def __init__(self, df):
 		self.df = df
-		pass
 
 	"""
-	when(location): This method takes a location as an argument and returns
-	a list containing the years where games were held in the given location.
+	Takes a location as an argument and returns a list containing 
+	the years where games were held in the given location.
 	"""
 	def when(self, location):
+		if not isinstance(location, str):
+			return None
 		data_when = self.df[self.df.City == location]
 		# unique() prints 'year' values in order of appearance without duplicates
 		# it returns a panda array, so we cast it into a list
 		years = data_when['Year'].unique()
 		years = list(years)
-		return (years)
-		pass
+		return years
 
 	"""
-	where(date): This method takes a date as an argument and returns the
-	location where the Olympics took place in the given year.
+	Takes a date as an argument and returns the location 
+	where the Olympics took place in the given year.
 	"""
 	def where(self, date):
+		if not isinstance(date, int):
+			return None
 		data_where = self.df[self.df.Year == date]
-		return (data_where.City.iloc[0])
-		pass
+		cities = data_where['City'].unique()
+		cities = list(cities)
+		return cities
--- day04/ex04/test.py
+++ day04/ex04/test.py
@@ -1,30 +1,16 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    test.py                                            :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/28 19:35:38 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/29 20:40:17 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 from FileLoader import FileLoader
-import pandas as pd
+from SpatioTemporalData import SpatioTemporalData
 
-df = FileLoader()
-df = df.load("../resources/athlete_events.csv")
-year = 2016
-location = 'Athina'
-data_when = df[df.Year == year]
-data_where = df[df.City == location]
-# print(data_when)
-print(data_where)
-col = data_where['Year'].unique()
-col = list(col)
-print(col)
-print(type(col))
-data_when = data_when.City.iloc[0]
-print(data_when)
-print(type(data_when))
+loader = FileLoader()
+data = loader.load("../resources/athlete_events.csv")
+
+sp = SpatioTemporalData(data)
+
+print(sp.where(1896))
+print(sp.where(2016))
+print(sp.when('Athina'))
+print(sp.when('Paris'))
+
+print(sp.where(2000))
+print(sp.where(1980))
+print(sp.when('London'))
