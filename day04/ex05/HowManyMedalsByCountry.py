ec66d8d1100e611a7f5f22c970ff90e2fb15685a
Lamia <lboukrou@student.42.fr>
2022-07-05 16:37:27 +0200

    module04 ex05

--- day04/ex05/HowManyMedalsByCountry.py
+++ day04/ex05/HowManyMedalsByCountry.py
@@ -1,17 +1,34 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    HowManyMedalsByCountry.py                          :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: lboukrou <lboukrou@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/06/29 21:23:57 by lboukrou          #+#    #+#              #
-#    Updated: 2020/06/29 21:28:52 by lboukrou         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-from FileLoader import FileLoader
 import pandas as pd
 
-def HowManyMedalsByCountry(df, country):
-	
+
+def how_many_medals_by_country(df, country):
+    if not isinstance(df, pd.DataFrame) or not isinstance(country, str):
+        return None
+
+    team_sports = ['Basketball', 'Football', 'Tug-Of-War', 'Badminton', 'Sailing', 'Handball', 'Water Polo', 'Hockey',
+                   'Rowing', 'Bobsleigh', 'Softball', 'Volleyball', 'Synchronized Swimming', 'Baseball', 'Rugby Sevens',
+                   'Rugby', 'Lacrosse', 'Polo']
+    # Selects rows with corresponding country
+    data = df[df.Team == country]
+    # Selects rows corresponding to sports list
+    data_sport = data[data.Sport.isin(team_sports)]
+    # Remove duplicates
+    data_sport = data_sport.drop_duplicates(['Year', 'Medal', 'Event'])
+
+    # Selecting rows that don't belong to the sports list
+    data_others = data[~data.Sport.isin(team_sports)]
+    # Concatenate both dataframes
+    concat_data = pd.concat([data_sport, data_others])
+
+    years = concat_data['Year'].unique()
+    years = list(map(int, years))
+    years = sorted(years)
+    total = {}
+
+    for year in years:
+        data_year = concat_data[concat_data.Year == year]
+        total[year] = {'G': len(data_year[data_year.Medal == 'Gold']),
+                       'S': len(data_year[data_year.Medal == 'Silver']),
+                       'B': len(data_year[data_year.Medal == 'Bronze']),
+                       }
+    return total
