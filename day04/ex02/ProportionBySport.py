ada9606fc33ac4edf8cfe6f0e186c7e0d1a48f35
Lamia <lboukrou@student.42.fr>
2022-07-11 12:31:32 +0200

    [DEV] Change precision float

--- day04/ex02/ProportionBySport.py
+++ day04/ex02/ProportionBySport.py
@@ -6,10 +6,10 @@ import pandas as pd
 def ProportionBySport(df, olympic_year, olympic_sport, specific_gender):
 	"""
 	The function returns a float corresponding to the proportion (percentage)
-	of participants who played the givensport among the participants of the
+	of participants who played the given sport among the participants of the
 	given gender.
 	The function answers questions like the following : “What was the
-	percentage of female basketball players amongall the female participants
+	percentage of female basketball players among all the female participants
 	of the 2016 Olympics?”
 	"""
 	if not isinstance(df, pd.DataFrame):
@@ -25,8 +25,12 @@ def ProportionBySport(df, olympic_year, olympic_sport, specific_gender):
 	data_year = df[df.Year == year]
 	# number of athletes of specific gender in that specific year
 	data_year_gender = data_year[data_year.Sex == gender]
+	# drop duplicate of ID instead of name because it is more accurate
+	data_year_gender = data_year_gender.drop_duplicates(['ID'])
 	# number of a specific sport's athletes of specific gender
 	data_year_gender_sport = data_year_gender[data_year_gender.Sport == sport]
 	# dimensions of the dataframes
-	d = data_year_gender_sport.shape
-	return d[0] / data_year_gender.shape[0]
+	d = len(data_year_gender_sport.index)
+	res = d / len(data_year_gender.index)
+	res = ('%.3f' % res)
+	return res
